c4afae3a162cf3a6140f2ad4b3c08681d857559d
mathew <Adegbite>
2020-05-06 18:52:12 +0100

    Updates

---
+++ precache-manifest.e2b7b80749bfbd2434064a724ec10803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a4d85ea031cd985e7431f69be692fb",
+    "url": "/COVID-19---LIve/index.html"
+  },
+  {
+    "revision": "3aff3a8e77bd2385acb9",
+    "url": "/COVID-19---LIve/static/css/2.08c9d6ca.chunk.css"
+  },
+  {
+    "revision": "799d72836ed8006e08cb",
+    "url": "/COVID-19---LIve/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "3aff3a8e77bd2385acb9",
+    "url": "/COVID-19---LIve/static/js/2.e1d4079c.chunk.js"
+  },
+  {
+    "revision": "6e5e93577e0fa552b1f2bf4fa0ff88b3",
+    "url": "/COVID-19---LIve/static/js/2.e1d4079c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "799d72836ed8006e08cb",
+    "url": "/COVID-19---LIve/static/js/main.f6f37a84.chunk.js"
+  },
+  {
+    "revision": "deb8c49efdd0a4100258",
+    "url": "/COVID-19---LIve/static/js/runtime-main.ea76e1e9.js"
+  }
+]);
